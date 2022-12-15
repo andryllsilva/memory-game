b1a8617af0dee094fbcad42514c38ba959b8e255
andryllsilva <andryll.silva@outlook.com>
2022-12-15 17:48:10 -0300

    adicionado imagens laterais e modificações no js

--- src/js/game.js
+++ src/js/game.js
@@ -23,13 +23,51 @@ const createElement = (tag, className) => {
 let firstCard = ''
 let secondCard = '';
 
+const checkWinner = () =>{
+    const disabledCards = document.querySelectorAll('.disable-card')
+
+    if(disabledCards.length === 20){
+        alert('Parabéns, você conseguiu!')
+    }
+}
+
+const checkCards = () => {
+    const firstCharacter = firstCard.getAttribute('data-character')
+    const secondCharacter = secondCard.getAttribute('data-character')
+
+    if(firstCharacter === secondCharacter){
+        firstCard.firstChild.classList.add('disable-card');
+        secondCard.firstChild.classList.add('disable-card');
+        firstCard = '';
+        secondCard = '';
+
+        checkWinner()
+    } else{
+        setTimeout(()=> {
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+        }, 500)
+    }
+}
+
 const revealCard = ({ target }) => {
     
     if(target.parentNode.className.includes('reveal-card')){
         return;
     }
 
-    target.parentNode.classList.add('reveal-card')
+    if(firstCard === ''){
+        target.parentNode.classList.add('reveal-card')
+        firstCard = target.parentNode;
+    } else if(secondCard === ''){
+        target.parentNode.classList.add('reveal-card')
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
 }
 
 const createCard = (character) => {
@@ -43,6 +81,7 @@ const createCard = (character) => {
     card.appendChild(back);
 
     card.addEventListener('click', revealCard)
+    card.setAttribute('data-character', character)
     
     return card;
 }
